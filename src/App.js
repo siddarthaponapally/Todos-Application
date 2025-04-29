fb206a2100326eeb2d7325e9f57f8d205dddd424
siddarthaponapally <siddarthaponapally000@gmail.com>
2025-04-29 12:21:56 +0530

    Add .gitattributes to normalize line endings

--- src/App.js
+++ src/App.js
@@ -1,25 +1,76 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import {v4 as uuidv4} from 'uuid'
 
-function App() {
+const App = () => {
+  const [userInput,setUserInput] = useState('')
+  const [tasks,setTasks] = useState([])
+  const [editText,setEditText] = useState('')
+  const [editTaskId,setEditTaskId] = useState(null)
+
+  const onAddButton = () => {
+    const newTask = {
+      text: userInput,
+      id : uuidv4(),
+    }
+    setTasks(prevTask => [...prevTask,newTask])
+    setUserInput('')
+  }
+
+  const onDelete = (taskToDelete) => {
+    const filteredResults = tasks.filter(task => 
+      task.id != taskToDelete.id
+    )
+    setTasks(filteredResults)
+  }
+
+  const onEdit = (task) => {
+    setEditTaskId(task.id)
+    setEditText(task.text)
+  }
+
+  const onSave = (taskId) => {
+    const updatedTasks = tasks.map(task =>
+      task.id === taskId ? {...task,text:editText} : task 
+    )
+    setTasks(updatedTasks)
+    setEditText('')
+    setEditTaskId(null)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1> My Task </h1>
+      <input type="text" placeholder='Enter your task' onChange={(event) => setUserInput(event.target.value)} value={userInput}/>
+      <button type='button' onClick={onAddButton}> Add </button>
+      <ul>
+        {tasks.map(task => (
+          <li key={task.id}>
+            {editTaskId === task.id ? (
+              <>
+                <input type='text' onChange={(event) => setEditText(event.target.value)} value={editText}/>
+                <button type='button' onClick={() => onSave(task.id)}> Save </button>
+              </>            
+            ): (
+              <>
+                {task.text}
+                <button type='button' onClick={() => onDelete(task)}> Delete </button>
+                <button type='button' onClick={() => onEdit(task)}> Edit </button>
+              </>
+            )} 
+          </li>
+        ))}
+      </ul>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
+
+
+
+
+
+
+
+
+
